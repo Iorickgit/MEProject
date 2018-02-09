625cfbfc6833e2e509573a9ae720958f5584e8ae
Iori Minami <iori.minami@gmail.com>
2018-02-10 03:06:25 +0900

    first commit

---
+++ MentorsEncyclopedia/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MentorsEncyclopedia
+//
+//  Created by 南伊織 on 2018/02/10.
+//  Copyright © 2018年 南伊織. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
